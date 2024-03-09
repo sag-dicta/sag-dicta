951a614c909b061bfe99c06b03257d9aec992a2e
Antonio Pérez <se7ens1ns94@gmail.com>
2024-03-09 16:10:39 -0600

    Analisis de Germinacion -- Filtros

--- MAS_PMSU/pages/AnalisisGerminacion.aspx.vb
+++ MAS_PMSU/pages/AnalisisGerminacion.aspx.vb
@@ -123,7 +123,6 @@ Public Class AnalisisGerminacion
             c4 = "AND ciclo_acta = '" & txtciclo.SelectedItem.Text & "' "
         End If
 
-
         Me.SqlDataSource1.SelectCommand = "SELECT " & cadena & " FROM `vista_acta_lote_multi` WHERE 1 = 1 AND fecha_acta IS NOT NULL AND estado_sena = '1' " & c1 & c3 & c4 & c2 & " ORDER BY id_acta DESC"
 
         GridDatos.DataBind()
